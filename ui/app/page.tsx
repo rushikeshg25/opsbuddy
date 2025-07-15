4b901fd34f15ce0978157448af3853e2af33f991
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-07-15 18:11:22 +0530

    feat: next app with tw v4 and shadcn

---
+++ ui/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <div>
+      <ThemeToggle />
+      <Button>Hi Bro</Button>
+    </div>
+  );
+};
+
+export default page;
